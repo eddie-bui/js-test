a8fd9df721daf32117126411b8cc89d751944e2b
Dat <buitandat.esmn@gmail.com>
2022-05-15 00:13:19 +0700

    change export

--- js/postApi.js
+++ js/postApi.js
@@ -1,19 +1,16 @@
 import fetchData from "./fetchApi";
 
-
-const postApi = {
-    getSuggestion() {
-        const url = 'https://api.json-generator.com/templates/dyj6ivO6Zbzv/data';
-        return fetchData(url)
-    },
-    getCollection() {
-        const url = 'https://api.json-generator.com/templates/JiITcIZCWfAu/data';
-        return fetchData(url)
-    },
-    getProduct() {
-        const url = 'https://api.json-generator.com/templates/jhOhdH17h06a/data';
-        return fetchData(url)
-    }
-}
-
-export default postApi
+export const postApi = {
+  getSuggestion() {
+    const url = "https://api.json-generator.com/templates/dyj6ivO6Zbzv/data";
+    return fetchData(url);
+  },
+  getCollection() {
+    const url = "https://api.json-generator.com/templates/JiITcIZCWfAu/data";
+    return fetchData(url);
+  },
+  getProduct() {
+    const url = "https://api.json-generator.com/templates/jhOhdH17h06a/data";
+    return fetchData(url);
+  },
+};
