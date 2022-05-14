d1bb52b18cf61acddabf3ed734700876eec3766f
Dat <buitandat.esmn@gmail.com>
2022-05-14 20:57:53 +0700

    code

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import postApi from "./postApi";
+
+function search() {
+  const searchInput = document.getElementById("searchBox");
+  if (!searchInput) return;
+
+  searchInput.addEventListener("input", (event) => {
+    var div = document.getElementById("searchResult");
+
+    listSuggestion = postApi.getSuggestion();
+    if (!listSuggestion.length) return;
+
+    div.style.display = "block";
+
+    let ulElement = document.getElementById("suggestions");
+    ulElement.innerHTML = "";
+    listSuggestion.forEach((item) => {
+      if (item.term.indexOf(event.target.value) >= 0) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(item.term));
+        li.setAttribute("id", item.id);
+        ulElement.appendChild(li);
+      }
+    });
+  });
+}
+search();
---
+++ js/postApi.js
@@ -0,0 +1,19 @@
+import fetchData from "./fetchApi";
+
+
+const postApi = {
+    getSuggestion() {
+        const url = 'https://api.json-generator.com/templates/dyj6ivO6Zbzv/data';
+        return fetchData(url)
+    },
+    getCollection() {
+        const url = 'https://api.json-generator.com/templates/JiITcIZCWfAu/data';
+        return fetchData(url)
+    },
+    getProduct() {
+        const url = 'https://api.json-generator.com/templates/jhOhdH17h06a/data';
+        return fetchData(url)
+    }
+}
+
+export default postApi
